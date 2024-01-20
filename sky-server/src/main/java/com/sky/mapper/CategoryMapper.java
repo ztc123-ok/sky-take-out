decbb46940c6972af896e1d7235175baaa9353db
ztc123-ok <2844692623@qq.com>
2024-01-20 15:45:44 +0800

    分类管理模块功能代码

---
+++ sky-server/src/main/java/com/sky/mapper/CategoryMapper.java
@@ -0,0 +1,50 @@
+package com.sky.mapper;
+
+import com.github.pagehelper.Page;
+import com.sky.dto.CategoryPageQueryDTO;
+import com.sky.entity.Category;
+import org.apache.ibatis.annotations.Delete;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+@Mapper
+public interface CategoryMapper {
+
+    /**
+     * 分页查询
+     * @param categoryPageQueryDTO
+     * @return
+     */
+    Page<Category> pageQuery(CategoryPageQueryDTO categoryPageQueryDTO);
+
+    /**
+     * 新增分类
+     * @param category
+     */
+    @Insert("insert into category(type,name,sort,status,create_time,update_time,create_user,update_user) " +
+            "values " +
+            "(#{type},#{name},#{sort},#{status},#{createTime},#{updateTime},#{createUser},#{updateUser})")
+    void save(Category category);
+
+    /**
+     * 删除分类
+     * @param id
+     */
+    @Delete("delete from category where id=#{id}")
+    void deleteById(Long id);
+
+    /**
+     * 根据类型查询分类
+     * @param type
+     * @return
+     */
+    List<Category> list(Integer type);
+
+    /**
+     * 更新分类
+     * @param category
+     */
+    void update(Category category);
+}
---
+++ sky-server/src/main/java/com/sky/service/CategoryService.java
@@ -0,0 +1,50 @@
+package com.sky.service;
+
+import com.sky.dto.CategoryDTO;
+import com.sky.dto.CategoryPageQueryDTO;
+import com.sky.entity.Category;
+import com.sky.result.PageResult;
+
+import java.util.List;
+
+public interface CategoryService {
+
+    /**
+     * 分类分页查询
+     * @param categoryPageQueryDTO
+     * @return
+     */
+    PageResult pageQuery(CategoryPageQueryDTO categoryPageQueryDTO);
+
+    /**
+     * 新增分类
+     * @param categoryDTO
+     */
+    void save(CategoryDTO categoryDTO);
+
+    /**
+     * 删除分类
+     * @param id
+     */
+    void deleteById(Long id);
+
+    /**
+     * 根据类型查询分类
+     * @param type
+     * @return
+     */
+    List<Category> list(Integer type);
+
+    /**
+     * 启用禁用分类
+     * @param status
+     * @param id
+     */
+    void startOrStop(Integer status, Long id);
+
+    /**
+     * 修改分类
+     * @param categoryDTO
+     */
+    void update(CategoryDTO categoryDTO);
+}

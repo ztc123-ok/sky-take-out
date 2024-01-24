fe103a8497ffdac48a5cb669ae366ce00eb374ec
ztc123-ok <2844692623@qq.com>
2024-01-24 17:03:56 +0800

    地址簿代码开发

---
+++ sky-server/src/main/java/com/sky/service/AddressBookService.java
@@ -0,0 +1,46 @@
+package com.sky.service;
+
+import com.sky.entity.AddressBook;
+
+import java.util.List;
+
+public interface AddressBookService {
+    /**
+     * 新增地址
+     * @param addressBook
+     */
+    void save(AddressBook addressBook);
+
+
+    /**
+     * 条件查询
+     * @param
+     * @return
+     */
+    List<AddressBook> list(AddressBook addressBook);
+
+    /**
+     * 根据id查询地址
+     * @param id
+     * @return
+     */
+    AddressBook getById(Long id);
+
+    /**
+     * 根据id删除地址
+     * @param id
+     */
+    void deleteById(Long id);
+
+    /**
+     * 根据id修改地址
+     * @param addressBook
+     */
+    void update(AddressBook addressBook);
+
+    /**
+     * 设置默认地址
+     * @param addressBook
+     */
+    void setDefault(AddressBook addressBook);
+}
---
+++ sky-server/src/main/java/com/sky/service/impl/AddressBookServiceImpl.java
@@ -0,0 +1,83 @@
+package com.sky.service.impl;
+
+import com.sky.context.BaseContext;
+import com.sky.entity.AddressBook;
+import com.sky.mapper.AddressBookMapper;
+import com.sky.service.AddressBookService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@Slf4j
+public class AddressBookServiceImpl implements AddressBookService {
+
+    @Autowired
+    private AddressBookMapper addressBookMapper;
+    /**
+     * 新增地址
+     * @param addressBook
+     */
+    @Override
+    public void save(AddressBook addressBook) {
+        addressBook.setUserId(BaseContext.getCurrentId());
+        addressBook.setIsDefault(0);
+        addressBookMapper.insert(addressBook);
+    }
+
+    /**
+     * 条件查询
+     * @param
+     * @return
+     */
+    @Override
+    public List<AddressBook> list(AddressBook addressBook) {
+        List<AddressBook> list = addressBookMapper.list(addressBook);
+        return list;
+    }
+
+    /**
+     * 根据id查询地址
+     * @param id
+     * @return
+     */
+    @Override
+    public AddressBook getById(Long id) {
+        AddressBook addressBook = addressBookMapper.getById(id);
+        return addressBook;
+    }
+
+    /**
+     * 根据id删除地址
+     * @param id
+     */
+    @Override
+    public void deleteById(Long id) {
+        addressBookMapper.deleteById(id);
+    }
+
+    /**
+     * 根据id修改地址
+     * @param addressBook
+     */
+    @Override
+    public void update(AddressBook addressBook) {
+        addressBookMapper.update(addressBook);
+    }
+
+    @Override
+    public void setDefault(AddressBook addressBook) {
+        //1、将当前用户的所有地址修改为非默认地址 update address_book set is_default = ? where user_id = ?
+        addressBook.setIsDefault(0);
+        addressBook.setUserId(BaseContext.getCurrentId());
+        addressBookMapper.updateIsDefaultByUserId(addressBook);
+
+        //2、将当前地址改为默认地址 update address_book set is_default = ? where id = ?
+        addressBook.setIsDefault(1);
+        addressBookMapper.update(addressBook);
+    }
+
+
+}
